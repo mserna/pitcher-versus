d87eaa7e945c823d37b3ae8663fc7279f5603878
mserna <matthewserna714@gmail.com>
2024-04-11 21:42:47 -0700

    Updates

---
+++ electron.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow } = require('electron');
+const isDev = require('electron-is-dev');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        show: false,
+    });
+
+    const startUrl = isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`;
+
+    mainWindow.loadURL(startUrl);
+    mainWindow.once('ready-to-show', () => mainWindow.show());
+    mainWindow.in('closed', () => {
+        mainWindow = null;
+    });
+}
+
+app.on('ready', createWindow);
